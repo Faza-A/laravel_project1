d5f884a7c17171b876ca30678380d18c28c6916f
fazaafrizal97 <faza.afrizal27@gmail.com>
2021-04-16 08:18:07 +0800

    Technical Training 5

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CountryController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\Api\CountryAPIController;
+use App\Http\Controllers\Api\UserAPIController;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use App\Http\Resources\CountryResource;
+use App\Models\Country;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,8 +24,14 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('countries', [CountryController::class, 'api']);
+Route::get('json', function(){
+    $user = User::find(1);
+    return new UserResource($user);
+});
 
-Route::apiResource("users", UserController::class);
-Route::post('/login', [UserController::class, 'login']);
-Route::get('/login', [UserController::class, 'login']);
+Route::apiResource('countries', CountryAPIController::class);
+
+Route::apiResource("users", UserAPIController::class);
+
+Route::post('/login', [UserAPIController::class, 'login']);
+Route::get('/login', [UserAPIController::class, 'login']);
